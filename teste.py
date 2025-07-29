9d00f4c6cf01595da2130bb3a7b483a308488d75
VascoPereira1 <127115718+VascoPereira1@users.noreply.github.com>
2025-07-29 16:31:54 +0100

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,6 @@
+import psutil
+print("CPU usage (%):", psutil.cpu_percent(interval=1))
+
+ram = psutil.virtual_memory()
+print("RAM usage (%):", ram.percent)
+print("RAM used (GB):", round(ram.used / 1e9, 2))
